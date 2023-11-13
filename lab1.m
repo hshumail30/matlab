fe8c64feeaad259c3e767716bd151b450157d3a0
hshumail30 <148746289+hshumail30@users.noreply.github.com>
2023-11-13 12:33:06 +0500

    Add files via upload

---
+++ lab1.m
@@ -0,0 +1,20 @@
+array1=[1 2 3 4 5 6]
+array2=[1 2 3; 4 5 6; 7 8 9]
+array3=[1;2;3;4]
+v=[1 3,sqrt(5)]
+v2=[3+ 4 5]
+v3=[3 4 5]
+array3=0:5
+start=0;
+step=0.25;
+stop=4;
+array4=start:step:stop
+sum4=sum(array4)
+sin(pi/4)
+sin([0*pi/4*pi/23*pi/4*pi])
+u=zeros(1, 10)
+u(5)=1
+stem(u)
+t=-1:.0005:1;
+xt=sin(2* pi* 5.*t +pi/6);
+plot(t,xt,'b-')
